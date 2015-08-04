9503ae12db79cd90eea1bde692664e1cfac0df90
James Church <jcchurch@gmail.com>
2015-08-04 12:24:15 -0400

    Added documentation to pull_post_data

--- garden-intern-manage-applicants/includes/pull_post_data.php
+++ garden-intern-manage-applicants/includes/pull_post_data.php
@@ -1,5 +1,12 @@
 <?php
 
+/**
+ * Returns the active identifier that should be examined
+ * or updated. If there is no active identifier, returns
+ * -1.
+ *
+ * @return the active identifier or -1
+ */
 function pullActiveRecordIdentifier() {
     /* If there is no active post, do nothing. */
     if (!isset($_POST['active'])) {
@@ -15,6 +22,12 @@ function pullActiveRecordIdentifier() {
     return $recordId;
 }
 
+/**
+ * Returns the staff comments. If there are no staff
+ * comments, returns an empty string.
+ *
+ * @return the staff comments or an empty string.
+ */
 function pullStaffComments() {
     if(isset($_POST['staff_comments'])) {
         return trim(sanitize_text_field($_POST['staff_comments']));
@@ -22,6 +35,12 @@ function pullStaffComments() {
     return "";
 }
 
+/**
+ * Returns the new status from the update form.
+ * If there is no new status, returns an empty string.
+ *
+ * @return the new status or an empty string.
+ */
 function pullNewStatus() {
     if (isset($_POST['status'])) {
         return $_POST['status'];
@@ -29,18 +48,40 @@ function pullNewStatus() {
     return "";
 }
 
+/**
+ * Returns true if the application list button was clicked.
+ *
+ * @return boolean if the application list button was clicked.
+ */
 function applicationListButtonClicked() {
     return isset($_POST['application_list']);
 }
 
+/**
+ * Returns true if the update button was clicked.
+ *
+ * @return boolean if the update button was clicked.
+ */
 function updateButtonClicked() {
     return isset($_POST['application_form']);
 }
 
+/**
+ * Returns true if the filter button was clicked.
+ *
+ * @return boolean if the filter button was clicked.
+ */
 function filterButtonClicked() {
     return isset($_POST['filter_button']);
 }
 
+/**
+ * Returns the list of criteria set forth from the
+ * filter form. If there is no criteria, returns an
+ * emtpy list.
+ *
+ * @return an array of criteria by which to filter (or an empty list)
+ */
 function pullFilterCriteria() {
     $filterCriteria = array();
 
