83fa2e13c31e19775400b96b3fd68a4d706f7595
James Church <jcchurch@gmail.com>
2015-08-05 10:05:59 -0400

    Changed [BLANK] to Select▾

--- garden-intern-manage-applicants/includes/display_selection_menu.php
+++ garden-intern-manage-applicants/includes/display_selection_menu.php
@@ -2,7 +2,7 @@
 
 /**
  * Displays a selection menu on the screen. The first option
- * is always "[BLANK]".
+ * is always "[BLANK]" and seen as "Select▾".
  *
  * @param $labal the lable of this selection menu
  * @param $name the internal name of this selection menus
@@ -11,7 +11,7 @@
 function displaySelectionMenu($label, $name, $options) {
     echo "$label\n";
     echo "<select name='$name'>\n";
-    echo "    <option value='[BLANK]'>[BLANK]</option>\n";
+    echo "    <option value='[BLANK]'>Select▾</option>\n";
 
     foreach ($options as $option) {
         echo "    <option value='$option'>$option</option>\n";
